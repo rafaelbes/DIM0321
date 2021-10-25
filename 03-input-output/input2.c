dcd69a8d31d9c9752c0725bc3a16899f283f102f
rafaelbes <rafaelufrn@gmail.com>
2021-10-25 18:58:10 -0300

    input2.c corrigido

--- 03-input-output/input2.c
+++ 03-input-output/input2.c
@@ -1,6 +1,16 @@
-int idade;
-float altura;
-printf("Digite a sua idade: ");
-scanf("%d", &idade);
-printf("Digite a sua altura: ");
-scanf("%f", &altura);
+#include <stdio.h>
+
+int main() {
+
+	float peso, altura;
+	float imc;
+	printf("Digite o seu peso: ");
+	scanf("%f", &peso);
+	printf("Digite a sua altura: ");
+	scanf("%f", &altura);
+	imc = peso/(altura*altura);
+	printf("O seu IMC eh: %f\n", imc);
+
+	return 0;
+}
+
